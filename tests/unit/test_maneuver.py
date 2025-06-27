f3b17be5b1ca6e66fd897651acda281c99581f4a
Zain Jabbar <zaijab2000@gmail.com>
2025-06-26 21:20:58 -0400

    Update everything

--- tests/unit/test_maneuver.py
+++ tests/unit/test_maneuver.py
@@ -2,7 +2,7 @@ import jax
 import jax.numpy as jnp
 
 from data_science_utils.dynamical_systems.crtbp import CR3BP
-from data_science_utils.filters import EnGMF
+from data_science_utils.filters import EnGMF, evaluate_filter
 from data_science_utils.filters.evaluate_imperfect_model import evaluate_filter_imperfect
 from data_science_utils.measurement_systems import Radar, RangeSensor
 from diffrax import Tsit5, ConstantStepSize
@@ -11,14 +11,14 @@ key = jax.random.key(1010)
 key, subkey = jax.random.split(key)
 
 
-dynamical_system = CR3BP(solver=Tsit5(), stepsize_contoller=ConstantStepSize())
+dynamical_system = CR3BP()
 measurement_system = RangeSensor()
 stochastic_filter = EnGMF()
 
 
 
 key, subkey = jax.random.split(key)
-rmse = evaluate_filter_imperfect(
+rmse = evaluate_filter(
     dynamical_system,
     measurement_system,
     stochastic_filter,
